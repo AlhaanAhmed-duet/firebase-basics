d217f381d3439cf7a83ce5355830143bccb8a34d
Alhaanahmed-duet <Alhaanahmed123@gmail.com>
2024-08-08 19:29:44 +0500

    Learned to getData

--- src/components/searchGames.jsx
+++ src/components/searchGames.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {collection, getDocs} from "firebase/firestore";
 import { db } from "../config/firebase-config";
 import { useEffect } from "react";
@@ -6,12 +7,20 @@ function searchGames() {
 
     const gamesCollectionRef = collection(db, "games");
 
+    const [gamesList, setGamesList] = useState([]);
 
     const getGameInfo = async () => {
         try {
         const data = await getDocs(gamesCollectionRef);
-        console.log(data);
+        const filteredData = data.docs.map((doc) => {
+            return {...doc.data(),
+                    id: doc.id,
+            }
+        });
+        console.log(filteredData);
+        setGamesList(filteredData);
         }
+        
         catch (err) {
             console.error(err);
         }
@@ -30,6 +39,18 @@ function searchGames() {
         <>
             <input placeholder="Search by name.." />
             
+            <div>
+                {gamesList.map((game) => {
+                    return (
+                        <>
+                            <h1>Name: {game.name}</h1>
+                            <p>Year: {game.releaseData}</p>
+                            <p>{game.avaialbleOnPC ? "For Windows" : "Windows not supported"}</p>
+                            <div>{game.availableOnAndroid ? "For Android" : "Android Not Supported"}</div>
+                        </>
+                    )
+                })}
+            </div>
         </>
     )
 }
